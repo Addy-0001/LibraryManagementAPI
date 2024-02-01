7abb89615ec4619ecb3bb1153128dd5e4f3bd43a
Adamya Neupane <adamsneupane@gmail.com>
2024-02-01 21:46:18 +0545

    final

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('api/', )
+    path('list-users/', views.UserListView.as_view(), name="list-users"),
+    path('create-users/', views.UserCreateView.as_view(), name='create-users'),
+    path('user/<int:ID>/', views.UserDetailView, name='user-detail-view'),
+    path('list-books/', views.BookListView.as_view(), name="list-books"),
+    path('create-books/', views.BookCreateView.as_view(), name='create-books'),
+    path('book-detail/<int:ID>/', views.BookDetailView, name='book-detail-view'),
+    path('book-detail-update/<int:BookID>/',
+         views.BookDetailRetriveUpdateDestroy.as_view(), name="book-detail-update"),
+    path('create-book-detail/', views.CreateBookDetail.as_view(),
+         name='create-book-detail'),
+    path('return-book/<int:BookID>/',
+         views.ReturnBookView.as_view(), name='return-book'),
+    path('list-borrowed-books/', views.ListBurrowedBooks.as_view(),
+         name='list-borrowed-books'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,4 +2,77 @@ from django.shortcuts import render
 from .serializers import UserModelSerializer, BookModelSerializer, BookDetailModelSerializer, BorrowedBooksModelSerializer
 from user.models import UserModel
 from books.models import Book, BookDetail, BorrowedBooks
-# Create your views here.
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+class UserListView(ListAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserModelSerializer
+
+
+class UserCreateView(CreateAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserModelSerializer
+
+
+@api_view()
+def UserDetailView(request, ID):
+    queryset = UserModel.objects.get(UserID=ID)
+    return Response({
+        'UserID': queryset.UserID,
+        'Name': queryset.Name,
+        'Email': queryset.Email,
+        'MembershipDate': queryset.MembershipDate
+    })
+
+
+class BookListView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+
+class BookCreateView(CreateAPIView):
+    queryset = Book
+    serializer_class = BookModelSerializer
+
+
+@api_view(['GET'])
+def BookDetailView(request, ID):
+    queryset = Book.objects.get(BookID=ID)
+    return Response({
+        'BookID': queryset.BookID,
+        "Title": queryset.Title,
+        "ISBN": queryset.ISBN,
+        "Genre": queryset.Genre,
+        "PublishedDate": queryset.PublishedDate,
+    })
+
+
+class CreateBookDetail(CreateAPIView):
+    queryset = BookDetail.objects.all()
+    serializer_class = BookDetailModelSerializer
+
+
+class BookDetailRetriveUpdateDestroy(RetrieveUpdateAPIView):
+    queryset = BookDetail.objects.all()
+    lookup_url_kwarg = 'BookID'
+    serializer_class = BookDetailModelSerializer
+
+
+class BorrowBookView(CreateAPIView):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksModelSerializer
+
+
+class ReturnBookView(DestroyAPIView):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksModelSerializer
+    lookup_url_kwarg = 'BookID'
+
+
+class ListBurrowedBooks(ListAPIView):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksModelSerializer
